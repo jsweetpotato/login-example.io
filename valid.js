a58ede1ae3e4e8bbf6654eb30a7c15db991ff7f0
jisu kim <lerry37@naver.com>
2021-05-27 02:25:10 +0900

    message1

---
+++ valid.js
@@ -0,0 +1,53 @@
+function emailValidation(){
+    let form = document.getElementById("form");
+    let email = document.getElementById("email").value;
+    let text = document.getElementById("email-text");
+    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    let valid = "emailValid"
+    let invalid = "emailInvalid"
+
+    if (email.match(pattern)) {
+        form.classList.add(valid);
+        form.classList.remove(invalid);
+        text.innerHTML = "Your Email Address in Valid."
+        text.style.color = "#00ff00"
+    } else{
+        form.classList.remove(valid);
+        form.classList.add(invalid);
+        text.innerHTML = "Please Enter Valid Email Address"
+        text.style.color = "#ff0000"
+    }
+    if (email == "") {
+        form.classList.remove(valid);
+        form.classList.remove(invalid);
+        text.innerHTML = ""
+        text.style.color = "#00ff00"
+    } 
+}
+
+function pwValidation(){
+    let form = document.getElementById("form");
+    let password = document.getElementById("password").value;
+    let text = document.getElementById("password-text");
+    let pattern = /([A-Za-z,.!?]*){8,16}/;
+    let valid = "pwValid";
+    let invalid = "pwInvalid";
+
+    if (password.match(pattern)) {
+        form.classList.add(valid);
+        form.classList.remove(invalid);
+        text.innerHTML = "Your Email Address in Valid."
+        text.style.color = "#00ff00"
+    } else{
+        form.classList.remove(valid);
+        form.classList.add(invalid);
+        text.innerHTML = "Please Enter Valid Email Address"
+        text.style.color = "#ff0000"
+    }
+    if (password == "") {
+        form.classList.remove(valid);
+        form.classList.remove(invalid);
+        text.innerHTML = ""
+        text.style.color = "#00ff00"
+    } 
+}
